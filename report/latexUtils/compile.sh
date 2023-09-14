cfa46e77637e9b88166e2cc5a4ec05a0e4f44d44
nico8034 <nicolaika8034@gmail.com>
2023-09-15 00:13:00 +0200

    Add cleanup command to LateX compile script

--- report/latexUtils/compile.sh
+++ report/latexUtils/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$1" == "reflection" ]; then
-    cd /reflection && xelatex -synctex=1 -interaction=nonstopmode main.tex
+    cd /reflection && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
 elif [ "$1" == "groupreport" ]; then
-    cd /groupreport && xelatex -synctex=1 -interaction=nonstopmode main.tex
+    cd /groupreport && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
 else
-    cd /reflection && xelatex -synctex=1 -interaction=nonstopmode main.tex
-    cd /groupreport && xelatex -synctex=1 -interaction=nonstopmode main.tex
+    cd /reflection && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
+    cd /groupreport && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
 fi
